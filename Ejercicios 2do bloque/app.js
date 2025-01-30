00cc27a3ced0f2265b6f6f211422d0869e0ae96e
moisesfg-cloud <moisesfg.escuela@gmail.com>
2025-01-30 11:17:33 -0600

    Se trabajo en app.js

--- Ejercicios 2do bloque/app.js
+++ Ejercicios 2do bloque/app.js
@@ -1,7 +1,6 @@
 //Aqui se declara la variable que almacena el numero secreto generado en la funcion 
 let numeroSecreto = generarNumeroSecreto(); //Hambito o alcance de bloque
 
-console.log(numeroSecreto);
 
 //Este bloque asigna mediante un elemtento y el texto titutlo y parrafo
 function asignarTexoElemento(elemento,texto){
@@ -11,17 +10,23 @@ function asignarTexoElemento(elemento,texto){
 
 //Este bloque te hace saber mediante una alerta que hiciste click en el botón
 //Asi se crean funciones
-function intentoDeUsuario(){
-    alert('Click desde el botón');
+function verificarIntento(){
+    //Estamos buscando elementos por su id, posterior a eso podemos tambien obtener los valores y al mismo tiempo se esta convirtiendo el valor en nùmero integer 
+    let  numeroDeUsuario = parseInt(document.getElementById('numeroUsuario').value);
+    console.log(typeof(numeroDeUsuario));
+    console.log(numeroDeUsuario);
+    console.log(typeof(numeroSecreto));
+    console.log(numeroSecreto);
+    // === esto significa que a la hora de compararse debe de ser igual en valor
+    console.log(numeroDeUsuario === numeroSecreto);
     return;
 }
 
 //Este bloque genera el número secreto
 function generarNumeroSecreto(){
-    return Math.floor(Math.random()* 10)+1; 
+    return Math.floor(Math.random()* 10)+1;
   }
-
 //Este bloque es la continuación desde la linea 5 a la 9
 asignarTexoElemento('h1','Bienvenido al juego del número secreto') //De esta manera automatizamos el proceso de estar definiendo variables y por medio de una sola funcion podemos hacer todo 
-asignarTexoElemento('p','Ingresa un número del 1 al 10')
+asignarTexoElemento('p','Ingresa un número del 1 al 10: ')
 
