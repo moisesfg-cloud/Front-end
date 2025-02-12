89ede3373283b6235d5a30147d3fd807ac94c3ec
moidev <moisesfg.escuela@gmail.com>
2025-02-11 21:18:10 -0600

    Trabajando en bloque 2

--- Ejercicios 2do bloque/app.js
+++ Ejercicios 2do bloque/app.js
@@ -21,8 +21,8 @@ function asignarTexoElemento(elemento,texto){
 //Este bloque te hace saber mediante una alerta que hiciste click en el botón
 //Asi se crean funciones
 function verificarIntento(){
-    //Estamos buscando elementos por su id, posterior a eso podemos tambien obtener los valores y al mismo tiempo se esta convirtiendo el valor en nùmero integer 
-    let  numeroDeUsuario = parseInt(document.getElementById('numeroUsuario').value);
+    //Estamos buscando elementos por su id, posterior a eso podemos tambien obtener los valores y al mismo tiempo se esta convirtiendo el valor en nùmero integer
+    let numeroDeUsuario = parseInt(document.getElementById('numeroUsuario').value);
     //Imprimimos que tipo de dato es
     console.log(typeof(numeroDeUsuario));
     //Imprimimos en consola que numero nos aparece
@@ -34,14 +34,15 @@ function verificarIntento(){
     // === esto significa que a la hora de compararse debe de ser igual en valor
     console.log(numeroDeUsuario === numeroSecreto);
     return;
-}
+    }
 
 //Este bloque genera el número secreto
 function generarNumeroSecreto(){
     return Math.floor(Math.random()* 10)+1;
-  }
+  } 
+  
 //Este bloque es la continuación desde la linea 5 a la 9
-asignarTexoElemento('h1','Bienvenido al juego del número secreto') //De esta manera automatizamos el proceso de estar definiendo variables y por medio de una sola funcion podemos hacer todo 
+asignarTexoElemento('h1','Bienvenido al juego del número secreto') //De esta manera automatizamos el proceso de estar definiendo variables y por medio de una sola funcion podemos hacer todo
 asignarTexoElemento('p','Ingresa un número del 1 al 10: ')
 
 
